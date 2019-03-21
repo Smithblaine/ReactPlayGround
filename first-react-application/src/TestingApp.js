52eb5b838c19d10c89d0e543ea722adea4a44339
Blaine Smith <Ringospy@gmail.com>
2019-03-20 20:00:54 -0500

    Adding jquery and slowly learning to reder items to the page

---
+++ first-react-application/src/TestingApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class TestingApp extends Component {
+    render() {
+      return (
+        <div id='App'>
+          <h1>Hello world</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default TestingApp;
